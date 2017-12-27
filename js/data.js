1bb4500386237cf1ec03d19d36b415c0d45787ef
yoli21 <y_venturchz@hotmail.com>
2017-12-26 22:32:10 -0500

    avances

---
+++ js/data.js
@@ -0,0 +1,154 @@
+var data = {
+  'MIRAF': {
+      'restaurantes': [{
+        'name': 'RESTAURANTE CEVICHERIA ALVA',
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+        'direccion': 'AV. CARLOS MELLET MZA. F1 LOTE. 2 URB. SAN JUDAS TADEO',
+        'whatsapp': 999125352,
+        'horarios': 'LUNES - JUE de 11 am -9 pm / VIE-DOM de 11am - 10 pm',
+        'detalles': [{
+            'platoBandera': 'SUBMARINO',
+            'platos': {
+              'plato1': 'Ceviche de mariscos',
+              'plato2': 'Ceviche de calamares',
+              'plato3': 'Arroz con mariscos',
+              'plato4': 'Parihuelas'
+            }
+          }
+
+        ]
+      },
+      {
+        'name': 'RESTAURANT DEL CHARAPITA',
+        'photo': 'http://dummyimage.com/102x187.png/dddddd/000000',
+        'direccion': 'CAL.CORONEL CANO NRO. 111 URB. SAN JUAN',
+        'whatssap': 989425312,
+        'horarios': 'LUN-DOM de 11 am -3 pm / 7pm - 11 pm',
+        'detalles': [{
+            'platoBandera': 'CEZINA A LO MACHO',
+            'platos': {
+              'plato1': 'Chaufa de cezina',
+              'plato2': 'Ensalada de Chonta o Plamito',
+              'plato3': 'Juanes de gallina',
+              'plato4': 'Brochetas de SURI'
+            }
+          }
+
+        ]
+      },
+      {
+        'name': 'PEÑA DEL PEZ NORTEÑO',
+        'photo': 'http://dummyimage.com/102x187.png/dddddd/000000',
+        'direccion': 'AV. RICHARDSON 171',
+        'whatssap': 993869457,
+        'horarios': 'LUN-DOM de 11 am -3 pm / 7pm - 11 pm',
+        'detalles': [{
+            'platoBandera': 'SECO DE CORDERO',
+            'platos': {
+              'plato1': 'La Malarrabia',
+              'plato2': 'El Shambar',
+              'plato3': 'Arroz con pato a la chiclayana',
+              'plato4': 'Ceviche de conchas negras'
+            }
+          }
+
+        ]
+      },
+      {
+        'name': 'LAS DELICIAS DEL SURCHICO',
+        'photo': 'http://dummyimage.com/102x187.png/dddddd/000000',
+        'direccion': 'AV. CARLOS MELLET MZA. F1 LOTE. 2 URB. SAN JUDAS TADEO',
+        'whatssap': 993869457,
+        'horarios': 'LUN-DOM de 11 am -3 pm / 7pm - 11 pm',
+        'detalles': [{
+            'platoBandera': 'ARROZ A LA CHACALA',
+            'platos': {
+              'plato1': 'choritos a la chalaca',
+              'plato2': 'Chaufa de mariscos',
+              'plato3': 'Carapulcra de chancho',
+              'plato4': 'Chicharrón'
+            }
+          }
+
+        ]
+      },
+    ]
+  
+  },
+
+  'CHORR': {
+    'restaurantes': [{
+        'name': 'RESTAURANTE CEVICHEIA ALVINO',
+        'photo': 'http://dummyimage.com/162x148.png/ff4444/ffffff',
+        'direccion': 'AV. CARLOS MELLET MZA. F1 LOTE. 2 URB. SAN JUDAS TADEO',
+        'whatsapp': 999125352,
+        'horarios': 'LUNES - JUE de 11 am -9 pm / VIE-DOM de 11am - 10 pm',
+        'detalles': [{
+            'platoBandera': 'SUBMARINO',
+            'platos': {
+              'plato1': 'Ceviche de mariscos',
+              'plato2': 'Ceviche de calamares',
+              'plato3': 'Arroz con mariscos',
+              'plato4': 'Parihuelas'
+            }
+          }
+
+        ]
+      },
+      {
+        'name': 'RESTAURANT DEL CHARAPO',
+        'photo': 'http://dummyimage.com/102x187.png/dddddd/000000',
+        'direccion': 'CAL.CORONEL CANO NRO. 111 URB. SAN JUAN',
+        'whatssap': 989425312,
+        'horarios': 'LUN-DOM de 11 am -3 pm / 7pm - 11 pm',
+        'detalles': [{
+            'platoBandera': 'CEZINA A LO MACHO',
+            'platos': {
+              'plato1': 'Chaufa de cezina',
+              'plato2': 'Ensalada de Chonta o Plamito',
+              'plato3': 'Juanes de gallina',
+              'plato4': 'Brochetas de SURI'
+            }
+          }
+
+        ]
+      },
+      {
+        'name': 'PEÑA DEL ROCOTO NORTEÑO',
+        'photo': 'http://dummyimage.com/102x187.png/dddddd/000000',
+        'direccion': 'AV. RICHARDSON 171',
+        'whatssap': 993869457,
+        'horarios': 'LUN-DOM de 11 am -3 pm / 7pm - 11 pm',
+        'detalles': [{
+            'platoBandera': 'SECO DE CORDERO',
+            'platos': {
+              'plato1': 'La Malarrabia',
+              'plato2': 'El Shambar',
+              'plato3': 'Arroz con pato a la chiclayana',
+              'plato4': 'Ceviche de conchas negras'
+            }
+          }
+
+        ]
+      },
+      {
+        'name': 'LAS DELICIAS DEL SUR',
+        'photo': 'http://dummyimage.com/102x187.png/dddddd/000000',
+        'direccion': 'AV. CARLOS MELLET MZA. F1 LOTE. 2 URB. SAN JUDAS TADEO',
+        'whatssap': 993869457,
+        'horarios': 'LUN-DOM de 11 am -3 pm / 7pm - 11 pm',
+        'detalles': [{
+            'platoBandera': 'ARROZ A LA CHACALA',
+            'platos': {
+              'plato1': 'choritos a la chalaca',
+              'plato2': 'Chaufa de mariscos',
+              'plato3': 'Carapulcra de chancho',
+              'plato4': 'Chicharrón'
+            }
+          }
+
+        ]
+      },
+    ]
+  }
+}
